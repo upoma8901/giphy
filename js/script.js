ea61d896a77a93467d5054e27448ec394846b920
upoma8901 <UPOMAB8901@QUEENSVOC.ORG>
2019-04-04 22:28:47 +0000

    d1

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@
 
 $("#search-button").click(function(){
 
-
  $.ajax({
       url:"https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
       method: "GET",
       success: function(response) {
+          console.log("HELLO");
          console.log(response);
       },
     });
