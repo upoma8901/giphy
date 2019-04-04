41e3ec05f2ca5da845e648cb692b0bd1f224cbe6
upoma8901 <UPOMAB8901@QUEENSVOC.ORG>
2019-04-04 22:18:29 +0000

    adding js

--- js/script.js
+++ js/script.js
@@ -4,14 +4,33 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  $.ajax({
-    url:"https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
-    method:"GET",
-    success: function(response) {
-        console.log(response);
-    },
-  });
+
+
+ $.ajax({
+      url:"https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+         console.log(response);
+      },
+    });
+
+   var input=$("#search-term").val();
+  var request_url = "https://api.giphy.com/v1/gifs/search?q="+input+ "&rating=pg&api_key=dc6zaTOxFJmzC";
+
+    $.ajax({
+      url: request_url,
+      method: "GET",
+      success: function(response) {
+           var pic_url = response.data[0].images.original.url;
+
+           $('.form-inline').html('<img src=' + pic_url + '>');
+      },
+    });
+
+
 
 
 });
+
+
 
