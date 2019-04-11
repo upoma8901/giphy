7fc35e35bf1f2eb82f44a2e5322bd69f075ad182
upoma8901 <UPOMAB8901@QUEENSVOC.ORG>
2019-04-11 21:17:20 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -5,14 +5,6 @@
 
 $("#search-button").click(function(){
 
- $.ajax({
-      url:"https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
-      method: "GET",
-      success: function(response) {
-          console.log("HELLO");
-         console.log(response);
-      },
-    });
 
    var input=$("#search-term").val();
   var request_url = "https://api.giphy.com/v1/gifs/search?q="+input+ "&rating=pg&api_key=dc6zaTOxFJmzC";
